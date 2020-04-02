d89978c87104482e234d896ab71635f682225b0c
Sajan <ksajan12@gmail.com>
2020-04-02 06:55:06 -0700

    perfected style of the login page

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput placeholder="Email/Username"
+      style={{borderColor: 'black', borderWidth: 0.8, padding: 5}} />
+      <TextInput placeholder="Password"
+      style={{borderColor: 'black', borderWidth: 0.8, padding: 5}} />
       <View style={styles.item1}>
         <Button title="Register" />
       </View>
@@ -18,20 +22,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
-    padding: 50,
+    padding: 70,
   },
   item1: {
     backgroundColor: '#f456',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50,
+    //alignItems: 'center',
+    justifyContent: 'center'
   },
   item2: {
     backgroundColor: '#f456',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 50,
+    //alignItems: 'center',
+    justifyContent: 'center'
   }
 });
