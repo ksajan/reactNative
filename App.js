873ce61630db3f003284366c03a749f90e039b8e
Sajan <ksajan12@gmail.com>
2020-04-02 11:26:54 -0700

    major update

--- App.js
+++ App.js
@@ -1,47 +1,88 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image, TouchableHighlight, TouchableOpacity } from 'react-native';
 
-export default function App() {
+
+export default class extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  }
+  render() {
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Email/Username"
-                      style={styles.input} />
-      <TextInput placeholder="Password"
-                      style={styles.input} />
-      <View style={styles.item1}>
-        <Button title="Login" />
-        <Button title="Register" />
+      <Text style={styles.logo}>LOL</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Email/Username"
+          style={styles.inputText}
+          onChangeText={text => this.setState({ email: text})} />
       </View>
-      <View style={styles.item2}>
+
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          style={styles.inputText}
+          onChangeText={text => this.setState({password: text})}/>
+
       </View>
+      <TouchableOpacity>
+        <Text style={styles.forgotPasswordText}>Forgot Password</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Register</Text>
+      </TouchableOpacity>
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
+    flex:1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
     justifyContent: 'center',
-    padding: 60
   },
-  item1: {
-    flex: 0,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 70,
+    color: '#fb5b5a',
+    marginBottom:70
   },
-  item2: {
-    backgroundColor: '#f456',
-    //alignItems: 'center',
-    justifyContent: 'center'
+  inputView: {
+    backgroundColor: '#465881',
+    justifyContent: 'center',
+    width: '80%',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    padding:20
   },
-  input: {
-    borderColor: 'black',
-    borderWidth: 0.8,
-    padding: 5,
+  inputText: {
+    height: 50,
+    color: 'white'
+  },
+  forgotPasswordText: {
+    color: 'white',
+    fontSize: 12,
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginTop: 40
+  },
+  loginText: {
     color: 'white'
   }
 });
